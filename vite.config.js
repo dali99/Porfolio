0af97e76a5108b9907898e093095c204df4af5f2
Dali1999 <mohamedali.magri@infinitymgt.fr>
2026-02-04 22:04:28 +0100

    fix: correct base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/REPO_NAME/', // ⚠️ exact GitHub repo name
+  base: '/', // ⚠️ exact GitHub repo name
 
 })
